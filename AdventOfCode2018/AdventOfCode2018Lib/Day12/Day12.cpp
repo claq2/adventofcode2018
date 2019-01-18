aef5052b882db3a93cc48efb5da675c4d635b6cd
James McLachlan <claquer@gmail.com>
2019-01-18 12:42:31 -0500

    Day 12 work

--- AdventOfCode2018/AdventOfCode2018Lib/Day12/Day12.cpp
+++ AdventOfCode2018/AdventOfCode2018Lib/Day12/Day12.cpp
@@ -39,72 +39,32 @@ tuple<vector<bool>, map<int, bool>> Day12::ReadInput()
 
 string Day12::Part1(tuple<vector<bool>, map<int, bool>> initialStateAndRules)
 {
-	const int padding(100);
 	auto state = get<0>(initialStateAndRules);
+	auto rules = get<1>(initialStateAndRules);
 	// Pad state with falses on both ends
+	vector <bool> paddingVector;
 	for (int i = 0; i < padding; i++)
 	{
-		state.insert(state.begin(), false);
-		state.push_back(false);
+		paddingVector.push_back(false);
 	}
+
+	state.insert(state.begin(), paddingVector.begin(), paddingVector.end());
+	state.insert(state.end(), paddingVector.begin(), paddingVector.end());
 
 	int plantPotsTotal(0);
 	string generations;
 
-	generations = BuildString(state);
+	generations = "0:" + BuildString(state);
 
 	for (int i = 0; i < 20; i++)
 	{
-		vector<bool> nextGeneration;
-		// Check from start + 2 to end - 2
-		for (auto it = state.begin() + 2; it != state.end() - 2; it++)
-		{
-			// Build bitmap of LLCRR
-			int bitmap(0);
-			for (int localIndex = -2; localIndex < 3; localIndex++)
-			{
-				if (*(it + localIndex))
-				{
-					bitmap += pow(2, localIndex + 2);
-				}
-			}
-
-			if (get<1>(initialStateAndRules).count(bitmap) && get<1>(initialStateAndRules)[bitmap])
-			{
-				nextGeneration.push_back(true);
-			}
-			else
-			{
-				nextGeneration.push_back(false);
-			}
-		}
-
-		// Re-pad ends with falses
-		for (int i = 0; i < 2; i++)
-		{
-			nextGeneration.insert(nextGeneration.begin(), false);
-			nextGeneration.push_back(false);
-		}
-
-		state = nextGeneration;
+		state = CalculateNextGeneration(state, rules);
 		generations += string("\r\n");
+		generations += to_string(i + 1) + ":";
 		generations += BuildString(state);
 	}
 
-	// Total up the plants' pot numbers
-	int index(-padding);
-	vector<int> nums;
-	for (auto const & s : state)
-	{
-		if (s)
-		{
-			plantPotsTotal += index;
-			nums.push_back(index);
-		}
-
-		index++;
-	}
-
+	plantPotsTotal = CalculatePotsTotal(state);
 	return to_string(plantPotsTotal);
 }
 
@@ -121,7 +81,142 @@ string Day12::BuildString(vector<bool> state)
 
 string Day12::Part2(tuple<vector<bool>, map<int, bool>> initialStateAndRules)
 {
-	return string();
+	auto state = get<0>(initialStateAndRules);
+	auto rules = get<1>(initialStateAndRules);
+	// Pad state with falses on both ends
+	vector <bool> paddingVector;
+	for (int i = 0; i < padding; i++)
+	{
+		paddingVector.push_back(false);
+	}
+
+	state.insert(state.begin(), paddingVector.begin(), paddingVector.end());
+	state.insert(state.end(), paddingVector.begin(), paddingVector.end());
+
+	long long plantPotsTotal(0);
+	string generations;
+
+	generations = "0:" + BuildString(state);
+	long long previousTotal(CalculatePotsTotal(state));
+	long long currentTotal(0);
+	long long previousDifference(currentTotal - previousTotal);
+	long long currentDifference(0);
+	long long generation(0);
+	long long previousCount(count(state.begin(), state.end(), true));
+	long long currentCount(0);
+	for (long long i = 0; i < 2000; i++)
+	{
+		state = CalculateNextGeneration(state, rules);
+		currentTotal = CalculatePotsTotal(state);
+		currentDifference = currentTotal - previousTotal;
+		currentCount = count(state.begin(), state.end(), true);
+		// Look for a repeating pattern of same number of plants changing by the same number each generation
+		if (currentCount == previousCount && currentDifference == previousDifference)
+		{
+			break;
+		}
+		else
+		{
+			generation++;
+			previousCount = currentCount;
+			previousDifference = currentDifference;
+			previousTotal = currentTotal;
+		}
+
+		generations += string("\r\n");
+		generations += to_string(i + 1) + ":";
+		generations += BuildString(state);
+	}
+
+	// Total up the plants' pot numbers
+	long long index(-padding);
+	//vector<long long> nums;
+	for (auto const & s : state)
+	{
+		if (s)
+		{
+			plantPotsTotal += index;
+			//nums.push_back(index);
+		}
+
+		index++;
+	}
+
+	return to_string(plantPotsTotal);
+}
+
+long long Day12::CalculatePotsTotal(vector<bool> state)
+{
+	long long plantPotsTotal(0);
+	// Total up the plants' pot numbers
+	long long index(-padding);
+	for (auto const & s : state)
+	{
+		if (s)
+		{
+			plantPotsTotal += index;
+		}
+
+		index++;
+	}
+
+	return plantPotsTotal;
+}
+
+vector<bool> Day12::CalculateNextGeneration(vector<bool> state, map<int, bool> rules)
+{
+	vector<bool> nextGeneration;
+	// Check from start + 2 to end - 2
+	for (auto it = state.begin() + 2; it != state.end() - 2; it++)
+	{
+		// Build bitmap of LLCRR
+		int bitmap(0);
+		for (int localIndex = -2; localIndex < 3; localIndex++)
+		{
+			if (*(it + localIndex))
+			{
+				bitmap += pow(2, localIndex + 2);
+			}
+		}
+
+		if (rules.count(bitmap) && rules[bitmap])
+		{
+			nextGeneration.push_back(true);
+		}
+		else
+		{
+			nextGeneration.push_back(false);
+		}
+	}
+
+	// Re-pad ends with falses
+	for (int i = 0; i < 2; i++)
+	{
+		nextGeneration.insert(nextGeneration.begin(), false);
+		nextGeneration.push_back(false);
+	}
+
+	//state = nextGeneration;
+	/*generations += string("\r\n");
+	generations += to_string(i + 1) + ":";
+	generations += BuildString(state);*/
+
+	//// Total up the plants' pot numbers
+	//long long index(-padding);
+	//long long localPlantPotsTotal(0);
+	////vector<long long> nums;
+	//for (auto const & s : state)
+	//{
+	//	if (s)
+	//	{
+	//		localPlantPotsTotal += index;
+	//		//nums.push_back(index);
+	//	}
+
+	//	index++;
+	//}
+
+	return nextGeneration;
 }
 
 tuple<vector<bool>, map<int, bool>> Day12::ParseInput(vector<string> lines)
--- AdventOfCode2018/AdventOfCode2018Lib/Day12/Day12.h
+++ AdventOfCode2018/AdventOfCode2018Lib/Day12/Day12.h
@@ -10,6 +10,9 @@ public:
 	std::string Part1(std::tuple<std::vector<bool>, std::map<int, bool>> initialStateAndRules);
 	std::string BuildString(std::vector<bool> state);
 	std::string Part2(std::tuple<std::vector<bool>, std::map<int, bool>> initialStateAndRules);
+	long long CalculatePotsTotal(std::vector<bool> state);
+	std::vector<bool> CalculateNextGeneration(std::vector<bool> state, std::map<int, bool> rules);
 	std::tuple<std::vector<bool>, std::map<int, bool>> ParseInput(std::vector<std::string> lines);
+	const long long padding = 2000;
 };
 
