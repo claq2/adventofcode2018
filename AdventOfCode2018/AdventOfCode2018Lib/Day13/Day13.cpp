e9f40738e5f7a28ff268985750de4ef3c3a594ca
James McLachlan <claquer@gmail.com>
2019-01-19 23:46:08 -0500

    Day 13 setup

--- AdventOfCode2018/AdventOfCode2018Lib/Day13/Day13.cpp
+++ AdventOfCode2018/AdventOfCode2018Lib/Day13/Day13.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "Day13.h"
 
+using namespace std;
 
 Day13::Day13()
 {
@@ -13,6 +14,24 @@ Day13::~Day13()
 
 std::string Day13::ReadInput()
 {
+	vector<string> lines;
+	ifstream file;
+	string currentLine;
+
+	file.open("Day13.txt");
+	if (!file)
+	{
+		exception e("file not open");
+		throw e;
+	}
+
+	tm tm = {};
+	while (getline(file, currentLine))
+	{
+		lines.push_back(currentLine);
+	}
+
+	file.close();
 	return std::string();
 }
 
