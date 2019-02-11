b691588f8d24a596e609878c74ceb2f240b52242
James McLachlan <claquer@gmail.com>
2019-02-11 08:15:09 -0500

    Day 13 solved. Day 14 setup.

---
+++ AdventOfCode2018/AdventOfCode2018Lib/Day14/Day14.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "../AdventOfCode2018Lib.h"
+
+class ADVENTOFCODE2018LIB_API Day14
+{
+public:
+	Day14() {}
+	~Day14() {}
+	int Day14::ReadInput()
+	{
+		return 513401;
+	}
+
+	std::string Day14::Part1(int input)
+	{
+
+	}
+
+	std::string Day14::Part2(int input)
+	{
+
+	}
+};
+
---
+++ AdventOfCode2018/MsUnitTest/Day14Tests.cpp
@@ -0,0 +1,36 @@
+﻿#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../AdventOfCode2018Lib/Day14/Day14.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace MsUnitTest
+{
+	TEST_CLASS(Day14Tests)
+	{
+	public:
+		
+
+		TEST_METHOD(ReadInput)
+		{
+			Day14 day14;
+			auto value(day14.ReadInput());
+			
+			Assert::AreEqual(513401, value);
+		}
+
+		TEST_METHOD(Part1)
+		{
+			Day14 day14;
+			
+		}
+
+		TEST_METHOD(Part2)
+		{
+			Day14 day14;
+			
+		}
+
+	};
+}
